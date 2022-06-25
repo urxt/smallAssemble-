c83e94f5640e5358734419c42405ea42b0b30de2
urxt <80124020+urxt@users.noreply.github.com>
2022-06-25 16:03:01 -0400

    updated gitignore and parsing by whitespace

--- assembler.py
+++ assembler.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python3
+import sys
 def main():
-    # read a file 
-    # write to a file 
+    tokens = []
+    with open("test.asm", 'r') as f:
+        for line in f.read():
+            tokens = line.split()
+            for i in tokens: 
+                print(i)
 
 
-
-if __name__ += "__main__":
+if __name__ == "__main__":
     main()
