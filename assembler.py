e6a23a8a2243ee2f7a799b7e57e5a29aa0aa8d03
urxt <80124020+urxt@users.noreply.github.com>
2022-06-25 18:27:59 -0400

    preparing file for parsing

--- assembler.py
+++ assembler.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
+
 import sys
+
 def main():
-    tokens = []
+    token = list()
     with open("test.asm", 'r') as f:
         for line in f.read():
-            tokens = line.split()
-            for i in tokens: 
-                print(i)
-
+            token = line.split()
+    f.close()
+    
 
 if __name__ == "__main__":
     main()
