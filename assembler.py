af0a1d956dcdf536b37c0ed3d30e9c7fe3325d88
urxt <80124020+urxt@users.noreply.github.com>
2022-06-25 21:45:09 -0400

    added function for parsing labels

--- assembler.py
+++ assembler.py
@@ -16,5 +16,18 @@ def check_file(filename):
         elif(tokens[0]) == "sd":
             print("STORE instruction")
 
+def parse_labels():
+    linenum = 0
+    with open("test.asm") as f:
+        for line in f:
+            line = line.strip().replace('\r', '') 
+            # Comments
+            if line[0] == '#':
+                continue 
+            if line[0] == '.':
+                labels[line[1:]] = linenum*4 
+            else:
+                linenum += 1
+
 if __name__ == "__main__":
     main()
